0baf0f8e5c1931432bb1ba8c14392a3e630b7a4e
Itoro James <kaytorah55@gmail.com>
2024-11-07 20:21:03 +0100

    new

--- second-semester/second-semester-assignmements/index.js
+++ second-semester/second-semester-assignmements/index.js
@@ -1,16 +1,21 @@
-const hamburger = document.querySelector(".navbar"); // Make sure you're selecting the <img> tag inside the navbar
-const ul = document.querySelector("ul");
+document.addEventListener("DOMContentLoaded", function () {
+  const flipCards = document.querySelectorAll(".flip-card");
+  const hamburger = document.querySelector(".navbar"); // Make sure you're selecting the <img> tag inside the navbar
+  const ul = document.querySelector("ul");
 
+  hamburger.addEventListener("click", () => {
+    if (hamburger.getAttribute("src") === "./assets/icon-hamburger (1).svg") {
+      hamburger.setAttribute("src", "./assets/icon-close-menu.svg");
+    } else {
+      hamburger.setAttribute("src", "./assets/icon-hamburger (1).svg");
+    }
 
-hamburger.addEventListener("click", () => {
-  // Check the current src of the hamburger icon
-  if (hamburger.getAttribute("src") === "./assets/icon-hamburger (1).svg") {
-    // Change to the close icon
-    hamburger.setAttribute("src", "./assets/icon-close-menu.svg");
-  } else {
-    // Change back to the hamburger icon
-    hamburger.setAttribute("src", "./assets/icon-hamburger (1).svg");
-  }
+    ul.classList.toggle("display");
+  });
 
-  ul.classList.toggle("display");
+  flipCards.forEach((card) => {
+    card.addEventListener("click", () => {
+      card.querySelector(".flip-card-inner").classList.toggle("rotate");
+    });
+  });
 });
